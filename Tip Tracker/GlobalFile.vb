004b2c3c29680fcd1ad4ea711ce0842a8c8e65c9
Matt Bailey <baile5mj@cmich.edu>
2020-07-03 17:17:05 -0400

    Clean up field name
    (cherry picked from commit 9ce1a292d3785762e7199db708e63c6b7064f62c)

--- Tip Tracker/GlobalFile.vb
+++ Tip Tracker/GlobalFile.vb
@@ -2,7 +2,7 @@
 
 Public Class GlobalFile
     Const _SETTINGS_FILE As String = "GlobalFile.location"
-    Private Shared _directory As String = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Tip Tracker")
+    Private Shared ReadOnly Directory As String = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Tip Tracker")
 
     ''' <summary>
     ''' Gets the path to the global data file.
@@ -10,13 +10,13 @@ Public Class GlobalFile
     ''' <returns>a string containing the path to the global file</returns>
     ''' <remarks></remarks>
     Public Shared Function GetGlobalFilePath() As String
-        Dim locationFilePath As String = Path.Combine(_directory, _SETTINGS_FILE)
+        Dim locationFilePath As String = Path.Combine(Directory, _SETTINGS_FILE)
         Dim reader As StreamReader
         Dim location As String = ""
 
         If File.Exists(locationFilePath) Then
             Try
-                reader = New StreamReader(Path.Combine(_directory, _SETTINGS_FILE))
+                reader = New StreamReader(Path.Combine(Directory, _SETTINGS_FILE))
                 location = reader.ReadToEnd().Trim()
                 reader.Close()
                 reader.Dispose()
@@ -33,11 +33,11 @@ Public Class GlobalFile
     ''' </summary>
     ''' <remarks></remarks>
     Public Shared Sub SetGlobalFilePath(ByVal filePath As String)
-        If Not Directory.Exists(_directory) Then
-            Directory.CreateDirectory(_directory)
+        If Not IO.Directory.Exists(Directory) Then
+            IO.Directory.CreateDirectory(Directory)
         End If
 
-        Dim locationFilePath As String = Path.Combine(_directory, _SETTINGS_FILE)
+        Dim locationFilePath As String = Path.Combine(Directory, _SETTINGS_FILE)
         Dim writer As New StreamWriter(locationFilePath, False)
 
         Try
