64491b42e889e565121f9b82df3765280b0d0db8
Matt Bailey <baile1mj@gmail.com>
2020-07-19 15:01:38 -0400

    Create common project for shared dependencies

---
+++ Common/Data/DataTableErrorMessageBuilder.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Text
+
+Namespace Data
+    ''' <summary>
+    ''' Provides a method for building a message about errors contained in a DataTable.
+    ''' </summary>
+    Public Class DataTableErrorMessageBuilder
+        Private ReadOnly _table As DataTable
+
+        ''' <summary>
+        ''' Creates a new instance of the builder class.
+        ''' </summary>
+        ''' <param name="dataTable">The data table for which the message will be generated.</param>
+        Public Sub New(ByVal dataTable As DataTable)
+            If IsNothing(dataTable) Then _
+                Throw New ArgumentNullException(NameOf(DataSet), "Cannot determine errors from null table.")
+
+            _table = dataTable
+        End Sub
+
+        ''' <summary>
+        ''' Gets the error message for the DataTable.
+        ''' </summary>
+        ''' <returns>A string containing any errors present in the table.</returns>
+        Public Function GetErrorMessage() As String
+            Dim messageBuilder As New StringBuilder()
+
+            messageBuilder.AppendLine(BuildErrorMessageHeader())
+            messageBuilder.AppendLine(BuildRowErrors())
+
+            Return messageBuilder.ToString()
+        End Function
+
+        ''' <summary>
+        ''' Builds a header for the message.
+        ''' </summary>
+        ''' <returns>The error message header.</returns>
+        Private Function BuildErrorMessageHeader() As String
+            Dim headerBuilder As New StringBuilder()
+
+            headerBuilder.AppendLine(_table.TableName)
+            headerBuilder.AppendLine("==============================")
+
+            Return headerBuilder.ToString().Trim()
+        End Function
+
+        ''' <summary>
+        ''' Builds the error message for any rows containing errors.
+        ''' </summary>
+        ''' <returns>The row errors.</returns>
+        Private Function BuildRowErrors() As String
+            If Not _table.HasErrors Then Return "No errors"
+
+            Dim rowErrorBuilder As New StringBuilder()
+
+            For Each row As DataRow In _table.Rows
+                If Not row.HasErrors Then Continue For
+
+                rowErrorBuilder.AppendLine($"Error: {row.RowError}")
+
+                For Each column As DataColumn In _table.Columns
+                    Dim columnValue As Object = row(column.ColumnName)
+
+                    If IsDBNull(columnValue) Then columnValue = "NULL"
+
+                    rowErrorBuilder.AppendLine($"{vbTab}{column.ColumnName}: {columnValue}")
+                Next
+
+                rowErrorBuilder.AppendLine()
+            Next
+
+            Return rowErrorBuilder.ToString().TrimEnd()
+        End Function
+    End Class
+
+End Namespace
---
+++ Common/Data/PayPeriod/PayPeriodFile.vb
@@ -0,0 +1,83 @@
+﻿Imports System.IO
+
+Namespace Data.PayPeriod
+    ''' <summary>
+    ''' Provides methods for reading and writing pay period data files.
+    ''' </summary>
+    Public Class PayPeriodFile
+        Inherits ObfuscatedDataSetFile(Of FileDataSet)
+        Implements IDisposable
+
+        Private _fileStream As FileStream
+
+        ''' <summary>
+        ''' Creates a new instance of the pay period data file class.
+        ''' </summary>
+        ''' <param name="filePath">The path to the file in the file system.</param>
+        Public Sub New(ByVal filePath As String)
+            MyBase.New(filePath)
+        End Sub
+
+        ''' <summary>
+        ''' Opens the file with exclusive access.
+        ''' </summary>
+        Public Sub Open()
+            _fileStream = File.Open(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read)
+        End Sub
+
+        ''' <summary>
+        ''' Closes the file and releases the exclusive lock.
+        ''' </summary>
+        Public Sub Close()
+            _fileStream?.Close()
+        End Sub
+
+        ''' <summary>
+        ''' Reads the data from the file.
+        ''' </summary>
+        ''' <param name="unsafe">True to allow potentially unstable data to be read; otherwise, false.</param>
+        ''' <returns>The pay period data contained in the file.</returns>
+        Private Function ReadPayPeriodFile(ByVal unsafe As Boolean) As PayPeriodData
+            If IsNothing(_fileStream) Then
+                Throw New InvalidOperationException("The file has not been opened for reading.")
+            End If
+
+            Dim dataSet As FileDataSet = Read(_fileStream, unsafe)
+            dataSet.AcceptChanges()
+            Return PayPeriodData.Create(dataSet)
+        End Function
+
+        ''' <summary>
+        ''' Reads the data from the file.
+        ''' </summary>
+        ''' <returns>The pay period data contained in the file.</returns>
+        Public Function ReadPayPeriodFile() As PayPeriodData
+            Return ReadPayPeriodFile(False)
+        End Function
+
+        ''' <summary>
+        ''' Reads the data from the file without validating data relationships.
+        ''' </summary>
+        ''' <returns>The pay period data contained in the file.</returns>
+        ''' <remarks>This option could allow unstable data to be read in and should only
+        ''' be used for troubleshooting applications.</remarks>
+        Public Function ReadPayPeriodFileUnsafe() As PayPeriodData
+            Return ReadPayPeriodFile(True)
+        End Function
+
+        ''' <summary>
+        ''' Writes pay period data to the file.
+        ''' </summary>
+        ''' <param name="payPeriodData">The data to write.</param>
+        Public Overloads Sub Write(ByVal payPeriodData As PayPeriodData)
+            Write(payPeriodData.FileDataSet, _fileStream)
+            payPeriodData.FileDataSet.AcceptChanges()
+        End Sub
+
+        ''' <inheritdoc />
+        Public Sub Dispose() Implements IDisposable.Dispose
+            _fileStream?.Close()
+            _fileStream?.Dispose()
+        End Sub
+    End Class
+End Namespace
