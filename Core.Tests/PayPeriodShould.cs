63c657ff4f05c3c140eb967180a7c5bdb25a1989
Matt Bailey <baile1mj@gmail.com>
2020-12-29 15:38:21 -0500

    Add pay period class

---
+++ Core.Tests/PayPeriodShould.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using TipTracker.Core;
+using Xunit;
+
+namespace Core.Tests
+{
+    public class PayPeriodShould
+    {
+        [Fact]
+        public void PreventCreatingInstanceWithStartDateAfterEndDate()
+        {
+            var start = DateTime.Today;
+            var end = start.AddDays(-1);
+
+            Assert.Throws<InvalidOperationException>(() => new PayPeriod(start, end, null));
+        }
+
+        [Fact]
+        public void CreateInstanceWithCorrectBusinessDateWhenSpecified()
+        {
+            var start = DateTime.Today;
+            var end = start.AddDays(7);
+            var businessDate = start.AddDays(2);
+            var testClass = new PayPeriod(start, end, businessDate, new List<Server>());
+
+            Assert.Equal(businessDate, testClass.BusinessDate);
+        }
+
+        [Fact]
+        public void CreateInstanceWithPeriodStartAsBusinessDateWhenNotSpecified()
+        {
+            var start = DateTime.Today;
+            var end = start.AddDays(7);
+            var testClass = new PayPeriod(start, end, new List<Server>());
+
+            Assert.Equal(start, testClass.BusinessDate);
+        }
+
+        [Fact]
+        public void CreateInstanceWithEmptyServerListWhenGivenNullList()
+        {
+            var testClass = new PayPeriod(DateTime.Today, DateTime.Today, null);
+
+            Assert.Empty(testClass.Servers);
+        }
+
+        [Fact]
+        public void CreateInstanceWithExpectedServerListWhenGivenServers()
+        {
+            var servers = new List<Server>
+            {
+                new Server { PosId = "1000", FirstName = "John", LastName = "Denver" },
+                new Server { PosId = "1001", FirstName = "George", LastName = "Jones" }
+            };
+            var testClass = new PayPeriod(DateTime.Today, DateTime.Today, servers);
+
+            Assert.Collection(testClass.Servers,
+                s => Assert.Same(servers[0], s),
+                s => Assert.Same(servers[1], s)
+            );
+        }
+
+        [Fact]
+        public void PreventCreatingInstanceWithBusinessDateBeforePeriodStart()
+        {
+            var start = DateTime.Today;
+            var end = start.AddDays(7);
+            var businessDate = start.AddDays(-1);
+
+            Assert.Throws<InvalidOperationException>(() => new PayPeriod(start, end, businessDate, new List<Server>()));
+        }
+
+        [Fact]
+        public void PreventCreatingInstanceWithBusinessDateAfterPeriodEnd()
+        {
+            var start = DateTime.Today;
+            var end = start.AddDays(7);
+            var businessDate = end.AddDays(1);
+
+            Assert.Throws<InvalidOperationException>(() => new PayPeriod(start, end, businessDate, new List<Server>()));
+        }
+
+        [Fact]
+        public void PreventSettingBusinessDateBeforePeriodStart()
+        {
+            var start = DateTime.Today;
+            var end = start.AddDays(7);
+            var testClass = new PayPeriod(start, end, new List<Server>());
+
+            Assert.Throws<InvalidOperationException>(() => testClass.BusinessDate = start.AddDays(-1));
+        }
+
+        [Fact]
+        public void PreventSettingBusinessDateAfterPeriodEnd()
+        {
+            var start = DateTime.Today;
+            var end = start.AddDays(7);
+            var testClass = new PayPeriod(start, end, new List<Server>());
+
+            Assert.Throws<InvalidOperationException>(() => testClass.BusinessDate = end.AddDays(1));
+        }
+    }
+}
---
+++ Core/PayPeriod.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipTracker.Core
+{
+    /// <summary>
+    /// Contains data about a pay period.
+    /// </summary>
+    public class PayPeriod
+    {
+        private readonly List<Server> _servers = new List<Server>();
+        private DateTime _businessDate;
+
+        /// <summary>
+        /// Gets the first day of the pay period.
+        /// </summary>
+        public DateTime Start { get; }
+
+        /// <summary>
+        /// Gets the last day of the pay period.
+        /// </summary>
+        public DateTime End { get; }
+
+        /// <summary>
+        /// Gets or sets the current business date.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If the specified business date falls outside the pay period.</exception>
+        public DateTime BusinessDate
+        {
+            get => _businessDate;
+            set
+            {
+                if (value < Start || value > End)
+                {
+                    throw new InvalidOperationException("The current date cannot be outside the pay period dates.");
+                }
+
+                _businessDate = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the collection of servers for the pay period.
+        /// </summary>
+        public IReadOnlyList<Server> Servers => _servers.AsReadOnly();
+
+        /// <summary>
+        /// Creates a new instance of the pay period.
+        /// </summary>
+        /// <param name="startDate">The first day of the pay period.</param>
+        /// <param name="endDate">The last day of the pay period.</param>
+        /// <param name="servers">The collection of servers who received tips in the period.</param>
+        public PayPeriod(DateTime startDate, DateTime endDate, List<Server> servers)
+            : this(startDate, endDate, startDate, servers)
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the pay period.
+        /// </summary>
+        /// <param name="startDate">The first day of the pay period.</param>
+        /// <param name="endDate">The last day of the pay period.</param>
+        /// <param name="businessDate">The current business date for the pay period.</param>
+        /// <param name="servers">The collection of servers who received tips in the period.</param>
+        public PayPeriod(DateTime startDate, DateTime endDate, DateTime businessDate, List<Server> servers)
+        {
+            // Ignore the time portion since it will only throw things off.
+            startDate = startDate.Date;
+            endDate = endDate.Date;
+            businessDate = businessDate.Date;
+
+            if (endDate < startDate)
+            {
+                throw new InvalidOperationException("The pay period cannot end before it begins.");
+            }
+
+            if (businessDate < startDate || businessDate > endDate)
+            {
+                throw new InvalidOperationException("The business date must be within the pay period.");
+            }
+
+            if (servers != null)
+            {
+                _servers.AddRange(servers);
+            }
+
+            Start = startDate;
+            End = endDate;
+            BusinessDate = businessDate;
+        }
+    }
+}
