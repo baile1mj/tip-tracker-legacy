05c3146a16d9ed1422441293360e8550da46d996
Matt Bailey <baile1mj@gmail.com>
2021-01-10 17:18:06 -0500

    Add class to contain strongly-defined tip types
    Rather than referring to tip types as string values, the strings will be encapsulated in a strongly-typed class.  Future development iterations will allow tip types to be defined by the users, so this is the first step in abstracting specific types.

---
+++ UI.WinForms/Utilities/TipTypes.vb
@@ -0,0 +1,40 @@
+﻿Namespace Utilities
+    ''' <summary>
+    ''' The collection of tip types supported by the application.
+    ''' </summary>
+    Public NotInheritable Class TipTypes
+        ''' <summary>
+        ''' Gets the name of the tip type.
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property Name As String
+
+        ''' <summary>
+        ''' Creates a new tip type.
+        ''' </summary>
+        ''' <param name="name">The name of the type.</param>
+        Private Sub New(name As String)
+            Me.Name = name
+        End Sub
+
+        ''' <summary>
+        ''' Defines tips received from credit card sales.
+        ''' </summary>
+        Public Shared CreditCard As New TipTypes("Credit Card")
+
+        ''' <summary>
+        ''' Defines tips received from room charge sales.
+        ''' </summary>
+        Public Shared RoomCharge As New TipTypes("Room Charge")
+
+        ''' <summary>
+        ''' Defines tips received from special events.
+        ''' </summary>
+        Public Shared SpecialFunction As New TipTypes("Special Function")
+
+        ''' <summary>
+        ''' Defines tips received from cash sales.
+        ''' </summary>
+        Public Shared Cash As New TipTypes("Cash")
+    End Class
+End Namespace
