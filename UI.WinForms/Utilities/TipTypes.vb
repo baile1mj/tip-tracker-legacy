f9d9fd7d6fd170f8592058d65def5f1eedf121c9
Matt Bailey <baile1mj@gmail.com>
2021-01-13 19:43:40 -0500

    Extend tip types class to encapsulate business logic
    Tip types will eventually be defined by the user, but the business logic will still apply.  Some payable tips (such as credit card and room charge) are accrued on a specific business date.  Claimed, non-payable tips (e.g. cash tips) are claimed for the pay period, and not for a specific date (though a date is required, so the last day of the pay period is used).  Tips accured from working an event are considered accrued on the date of the event, so the user cannot change the date for them.

--- UI.WinForms/Utilities/TipTypes.vb
+++ UI.WinForms/Utilities/TipTypes.vb
@@ -10,32 +10,53 @@
         Public ReadOnly Property Name As String
 
         ''' <summary>
+        ''' Gets a value indicating whether the user can set the date for this type.
+        ''' </summary>
+        ''' <returns>True if dates for tips of this type are use-specified; otherwise, false.</returns>
+        Public ReadOnly Property CanSpecifyDate As Boolean
+
+        ''' <summary>
+        ''' Gets a value indicating whether the tip originates from an event.
+        ''' </summary>
+        ''' <returns>True if tips of this type must be linked to an event; otherwise, false.</returns>
+        Public ReadOnly Property IsEventOriginated As Boolean
+
+        ''' <summary>
         ''' Creates a new tip type.
         ''' </summary>
         ''' <param name="name">The name of the type.</param>
-        Private Sub New(name As String)
+        ''' <param name="canSpecifyDate">True if the tip type uses a system-calculated date; otherwise, false.</param>
+        ''' <param name="isEventOriginated">True if the tip type originates from an event.</param>
+        Private Sub New(name As String, canSpecifyDate As Boolean, isEventOriginated As Boolean)
             Me.Name = name
+            Me.CanSpecifyDate = canSpecifyDate
+            Me.IsEventOriginated = isEventOriginated
         End Sub
+
+        ''' <inheritdoc/>
+        Public Overrides Function ToString() As String
+            Return Name
+        End Function
 
         ''' <summary>
         ''' Defines tips received from credit card sales.
         ''' </summary>
-        Public Shared CreditCard As New TipTypes("Credit Card")
+        Public Shared CreditCard As New TipTypes("Credit Card", True, False)
 
         ''' <summary>
         ''' Defines tips received from room charge sales.
         ''' </summary>
-        Public Shared RoomCharge As New TipTypes("Room Charge")
+        Public Shared RoomCharge As New TipTypes("Room Charge", True, False)
 
         ''' <summary>
         ''' Defines tips received from special events.
         ''' </summary>
-        Public Shared SpecialFunction As New TipTypes("Special Function")
+        Public Shared SpecialFunction As New TipTypes("Special Function", False, True)
 
         ''' <summary>
         ''' Defines tips received from cash sales.
         ''' </summary>
-        Public Shared Cash As New TipTypes("Cash")
+        Public Shared Cash As New TipTypes("Cash", False, False)
 
         ''' <summary>
         ''' Gets a collection containing all defined tip types.
