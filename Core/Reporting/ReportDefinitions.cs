48e6a5382bbcbf2c74a8c3045e47c05d81562fa9
Matt Bailey <baile1mj@gmail.com>
2021-12-29 10:02:34 -0500

    Add tip chit report

---
+++ Core/Reporting/ReportDefinitions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace TipTracker.Core.Reporting
+{
+    /// <summary>
+    /// Provides methods to load report definitions into memory streams. 
+    /// </summary>
+    public static class ReportDefinitions
+    {
+        /// <summary>
+        /// Gets the tip chip report definition stream.
+        /// </summary>
+        public static Stream TipChit => Assembly.GetAssembly(typeof(ReportDefinitions))
+                .GetManifestResourceStream("TipTracker.Core.Reporting.TipChit.rdlc");
+    }
+}
