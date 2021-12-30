6cd22ec274aeba3d6518bbb4a56808e196682060
Matt Bailey <baile1mj@gmail.com>
2021-12-30 16:43:18 -0500

    Clean up class and add XML documentation

--- Core/Reporting/TipTypeTotal.cs
+++ Core/Reporting/TipTypeTotal.cs
@@ -1,23 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TipTracker.Core.Reporting
+﻿namespace TipTracker.Core.Reporting
 {
+    /// <summary>
+    /// Represents a total for a tip type that will be included in a report.
+    /// </summary>
     public class TipTypeTotal
     {
+        /// <summary>
+        /// Gets the type of tip being totaled.
+        /// </summary>
         public TipType Type { get; }
 
+        /// <summary>
+        /// Gets the classification of the type being totaled.
+        /// </summary>
         public TipClassification Classification { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether tips of this type are claimed tips.
+        /// </summary>
         public bool IsClaimed { get; }
 
+        /// <summary>
+        /// Gets the count of all tips of this type.
+        /// </summary>
         public int Count { get; }
 
+        /// <summary>
+        /// Gets the total amount of all tips of this type.
+        /// </summary>
         public decimal Total { get; }
 
+        /// <summary>
+        /// Creates a new instance of the type total class.
+        /// </summary>
+        /// <param name="type">The type being totaled.</param>
+        /// <param name="count">The number of tips of this type.</param>
+        /// <param name="total">The total of all tips of this type.</param>
         public TipTypeTotal(TipType type, int count, decimal total)
         {
             Type = type;
@@ -26,7 +44,11 @@ namespace TipTracker.Core.Reporting
             Count = count;
             Total = total;
         }
-
+        
+        /// <summary>
+        /// Converts this instance to a loosely typed instance for reporting.
+        /// </summary>
+        /// <returns>An object representing this instance that can be passed to the reporting engine.</returns>
         public object ToAnonymous()
         {
             return new
