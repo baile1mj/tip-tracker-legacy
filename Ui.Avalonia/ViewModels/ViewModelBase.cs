6b265bcbf2c76fab61ef1d82138cd4926665ab67
Matt Bailey <baile1mj@gmail.com>
2024-08-08 09:53:44 -0400

    Add Avalonia UI project

---
+++ Ui.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TipTracker.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Ui.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TipTracker.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
