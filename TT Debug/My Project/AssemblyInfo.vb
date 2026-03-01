f88dc11d8e495800c121dc1cc20fac2f54f2e5a8
Matt Bailey <baile1mj@gmail.com>
2026-03-01 18:39:53 -0500

    Update targed framework version

--- TT Debug/My Project/AssemblyInfo.vb
+++ TT Debug/My Project/AssemblyInfo.vb
@@ -11,14 +11,14 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("Tip Tracker Debugger")> 
 <Assembly: AssemblyDescription("Provides methods for debugging Tip Tracker data files and global files.")> 
 <Assembly: AssemblyCompany("MJB")> 
-<Assembly: AssemblyProduct("Tip Tracker Debugger")> 
-<Assembly: AssemblyCopyright("Copyright ©  2008")> 
+<Assembly: AssemblyProduct("Tip Tracker Debugger")>
+<Assembly: AssemblyCopyright("Copyright ©  2008-2026")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("83c742ab-bb28-4568-9877-c9285cfa17c2")> 
+<Assembly: Guid("83c742ab-bb28-4568-9877-c9285cfa17c2")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.0.0.0")> 
-<Assembly: AssemblyFileVersion("2.0.0.0")> 
+<Assembly: AssemblyVersion("2.1.0.0")>
+<Assembly: AssemblyFileVersion("2.1.0.0")>
